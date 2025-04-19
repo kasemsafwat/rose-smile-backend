40bac4ebc23c6f3a376a996cec137c65f6a11629
Malik <malikhussein229@gmail.com>
2025-04-19 20:36:14 +0200

    Added the doctor router

--- src/index.Routes.ts
+++ src/index.Routes.ts
@@ -1,14 +1,16 @@
-import { Router } from "express";
-import authRouter from "./modules/auth/auth.routes";
-import userRouter from "./modules/user/user.routes";
-import sectionRouter from "./modules/section/section.routes";
-import serviceRouter from "./modules/service/service.routes";
-import offersRouter from "./modules/offers/offers.routes";
+import { Router } from 'express';
+import authRouter from './modules/auth/auth.routes';
+import userRouter from './modules/user/user.routes';
+import sectionRouter from './modules/section/section.routes';
+import serviceRouter from './modules/service/service.routes';
+import offersRouter from './modules/offers/offers.routes';
+import doctorRouter from './modules/doctor/doctor.routes';
 const router = Router();
-router.use("/auth", authRouter);
-router.use("/user", userRouter);
-router.use("/section", sectionRouter);
-router.use("/service", serviceRouter);
-router.use("/offer", offersRouter);
+router.use('/auth', authRouter);
+router.use('/user', userRouter);
+router.use('/section', sectionRouter);
+router.use('/service', serviceRouter);
+router.use('/offer', offersRouter);
+router.use('/doctor', doctorRouter);
 
 export default router;
