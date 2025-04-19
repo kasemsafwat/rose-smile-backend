2e1ee3e91d52f85c7b376727effe3f856dca558d
Malik <malikhussein229@gmail.com>
2025-04-19 20:36:00 +0200

    Added delete doctor, get all doctors and get doctor by id routes

--- src/modules/doctor/doctor.routes.ts
+++ src/modules/doctor/doctor.routes.ts
@@ -2,10 +2,15 @@ import { Router, RequestHandler } from 'express';
 import { valid } from '../../middleware/validation';
 import { isAuth } from '../../middleware/auth';
 import { Roles } from '../../DB/interfaces/user.interface';
-import { addDoctorSchema, updateDoctorSchema } from './doctor.valid';
+import {
+  addDoctorSchema,
+  updateDoctorSchema,
+  DoctorIdSchema,
+} from './doctor.valid';
 import Doctor from './service/doctor.service';
 import { configureMulter } from '../../utils/multer';
 import { asyncHandler } from '../../utils/errorHandling';
+import { getServicesSchema } from '../service/sevice.valid';
 
 const doctorRouter = Router();
 
@@ -32,6 +37,28 @@ doctorRouter.put(
   configureMulter(1024 * 1024 * 5).single('image'),
   isAuth([Roles.SuperAdmin]),
   asyncHandler(Doctor.updateDoctorImage)
+);
+
+// Delete a doctor
+doctorRouter.delete(
+  '/:doctorId',
+  valid(DoctorIdSchema) as RequestHandler,
+  isAuth([Roles.SuperAdmin]),
+  asyncHandler(Doctor.deleteDoctor)
+);
+
+// Get all doctors
+doctorRouter.get(
+  '/',
+  valid(getServicesSchema) as RequestHandler,
+  asyncHandler(Doctor.getAllDoctors)
+);
+
+// Get a doctor by ID
+doctorRouter.get(
+  '/:doctorId',
+  valid(DoctorIdSchema) as RequestHandler,
+  asyncHandler(Doctor.getDoctorById)
 );
 
 export default doctorRouter;
