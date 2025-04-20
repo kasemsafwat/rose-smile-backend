2faae451cd1c31c4bb2c8d88c2cd3100f2bb5394
Malik <malikhussein229@gmail.com>
2025-04-20 19:34:06 +0200

    added the form router

--- src/index.Routes.ts
+++ src/index.Routes.ts
@@ -5,6 +5,7 @@ import sectionRouter from './modules/section/section.routes';
 import serviceRouter from './modules/service/service.routes';
 import offersRouter from './modules/offers/offers.routes';
 import doctorRouter from './modules/doctor/doctor.routes';
+import formRouter from './modules/form/form.routes';
 const router = Router();
 router.use('/auth', authRouter);
 router.use('/user', userRouter);
@@ -12,5 +13,6 @@ router.use('/section', sectionRouter);
 router.use('/service', serviceRouter);
 router.use('/offer', offersRouter);
 router.use('/doctor', doctorRouter);
+router.use('/form', formRouter);
 
 export default router;
