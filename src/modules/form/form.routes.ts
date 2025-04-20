7bdcdd9719b33b28e43e04fe942687b547957adf
Malik <malikhussein229@gmail.com>
2025-04-20 19:33:58 +0200

    added routes to add a form and add a comment and update the form and delete and get by id and get all

---
+++ src/modules/form/form.routes.ts
@@ -0,0 +1,53 @@
+import { Router, RequestHandler } from 'express';
+import { valid } from '../../middleware/validation';
+import { isAuth } from '../../middleware/auth';
+import { Roles } from '../../DB/interfaces/user.interface';
+import {
+  addFormSchema,
+  addCommentSchema,
+  updateFormSchema,
+} from './form.valid';
+import Form from './service/form.service';
+import { asyncHandler } from '../../utils/errorHandling';
+
+const formRouter = Router();
+
+formRouter.post(
+  '/',
+  valid(addFormSchema) as RequestHandler,
+  asyncHandler(Form.addForm)
+);
+
+formRouter.patch(
+  '/:id',
+  isAuth([Roles.SuperAdmin, Roles.Admin]),
+  valid(addCommentSchema) as RequestHandler,
+  asyncHandler(Form.addComment)
+);
+
+formRouter.put(
+  '/:id',
+  isAuth([Roles.SuperAdmin]),
+  valid(updateFormSchema) as RequestHandler,
+  asyncHandler(Form.updateForm)
+);
+
+formRouter.delete(
+  '/:id',
+  isAuth([Roles.SuperAdmin]),
+  asyncHandler(Form.deleteForm)
+);
+
+formRouter.get(
+  '/:id',
+  isAuth([Roles.SuperAdmin, Roles.Admin]),
+  asyncHandler(Form.getFormById)
+);
+
+formRouter.get(
+  '/',
+  isAuth([Roles.SuperAdmin, Roles.Admin]),
+  asyncHandler(Form.getAllForms)
+);
+
+export default formRouter;
